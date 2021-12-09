551e81e2dd5f13c7029e095e428dfab3cba80ab2
Mellgood <carlocentos@gmail.com>
2021-12-09 15:53:57 +0100

    applicationContext added, Spring context

--- src/it/ntt/myfirstspring/App.java
+++ src/it/ntt/myfirstspring/App.java
@@ -1,5 +1,7 @@
 package it.ntt.myfirstspring;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Coach baseballCoach = new BaseballCoach();
@@ -7,6 +9,18 @@ public class App {
 
         System.out.println(baseballCoach.getDailyWorkout());
         System.out.println(trackCoach.getDailyWorkout());
+
+        System.out.println("#######################");
+
+        // utilizzo Spring
+        ClassPathXmlApplicationContext context = 
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Coach myCoach = context.getBean("myCoach", Coach.class);
+
+        System.out.println(myCoach.getDailyWorkout());
+
+        context.close();
         
     }
 }
