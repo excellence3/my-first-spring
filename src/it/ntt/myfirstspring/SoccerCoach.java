850cc0085b4633f4130bf360f836c5b2f69f0324
Mellgood <carlocentos@gmail.com>
2021-12-10 12:38:07 +0100

    added application.properties

--- src/it/ntt/myfirstspring/SoccerCoach.java
+++ src/it/ntt/myfirstspring/SoccerCoach.java
@@ -2,12 +2,19 @@ package it.ntt.myfirstspring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("prototype")
 public class SoccerCoach implements Coach {
+
+    @Value("${coach.name}")
+    private String name;
+
+    @Value("${coach.surname}")
+    private String surname;
 
     @Autowired
     @Qualifier("happyFortune")
@@ -16,9 +23,24 @@ public class SoccerCoach implements Coach {
 
     @Override
     public String getDailyWorkout() {
-        return "Kick the ball!  || " +
+        return name + " " + surname + ": Kick the ball!  || " +
             fortuneService.getFortune();
     }
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
   
 }
--- src/it/ntt/myfirstspring/config/ComponentScanConfig.java
+++ src/it/ntt/myfirstspring/config/ComponentScanConfig.java
@@ -2,9 +2,11 @@ package it.ntt.myfirstspring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("it.ntt.myfirstspring")
+@PropertySource("classpath:coach.properties")
 public class ComponentScanConfig {
     
 }
