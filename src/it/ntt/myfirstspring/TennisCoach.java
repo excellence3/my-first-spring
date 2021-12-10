694bc806da36578f7c4be19929f43adc768f14b4
Mellgood <carlocentos@gmail.com>
2021-12-10 11:08:39 +0100

    annotation fix

--- src/it/ntt/myfirstspring/TennisCoach.java
+++ src/it/ntt/myfirstspring/TennisCoach.java
@@ -1,11 +1,19 @@
 package it.ntt.myfirstspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach{
 
     private FortuneService fortuneService;
+
+    @Autowired
+    public TennisCoach(FortuneService fortuneService){
+        this.fortuneService = fortuneService;
+    }
+
+    public TennisCoach(){}
 
     @Override
     public String getDailyWorkout() {
